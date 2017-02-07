2442fd5008a365039a4aa586a1bf41dbd14cbd8d
brownae <aebrown9@gmail.com>
2017-02-06 21:23:31 -0800

    Uploaded a new item file so p2-2.php would work with itemsP-2.php

--- demo/p2-2.php
+++ demo/p2-2.php
@@ -1,21 +1,21 @@
 <?php
 /**
- * p2-1.php, based on demo_postback_nohtml.php is a single page web application that allows us to request and view 
+ * p2-1.php, based on demo_postback_nohtml.php is a single page web application that allows us to request and view
  * a customer's name
  *
  * This version uses no HTML directly so we can code collapse more efficiently
  *
- * This page is a model on which to demonstrate fundamentals of single page, postback 
+ * This page is a model on which to demonstrate fundamentals of single page, postback
  * web applications.
  *
- * Any number of additional steps or processes can be added by adding keywords to the switch 
+ * Any number of additional steps or processes can be added by adding keywords to the switch
  * statement and identifying a hidden form field in the previous step's form:
  *
  *<code>
  * <input type="hidden" name="act" value="next" />
  *</code>
- * 
- * The above live of code shows the parameter "act" being loaded with the value "next" which would be the 
+ *
+ * The above live of code shows the parameter "act" being loaded with the value "next" which would be the
  * unique identifier for the next step of a multi-step process
  *
  * @package ITC281
@@ -27,9 +27,9 @@
  * @todo add more complicated checkbox & radio button examples
  */
 
-# '../' works for a sub-folder.  use './' for the root  
+# '../' works for a sub-folder.  use './' for the root
 require '../inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials
-include 'items4.php'; 
+include 'itemsP-2.php'; 
 /*
 $config->metaDescription = 'Web Database ITC281 class website.'; #Fills <meta> tags.
 $config->metaKeywords = 'SCCC,Seattle Central,ITC281,database,mysql,php';
@@ -48,21 +48,21 @@ $config->nav1 = array("page.php"=>"New Page!") + $config->nav1; #add a new page 
 # Read the value of 'action' whether it is passed via $_POST or $_GET with $_REQUEST
 if(isset($_REQUEST['act'])){$myAction = (trim($_REQUEST['act']));}else{$myAction = "";}
 
-switch ($myAction) 
+switch ($myAction)
 {//check 'act' for type of process
 	case "display": # 2)Display user's name!
 	 	showData();
 	 	break;
-	default: # 1)Ask user to enter their name 
+	default: # 1)Ask user to enter their name
 	 	showForm();
 }
 
 function showForm()
 {# shows form so user can enter their name.  Initial scenario
 	global $config;
-    get_header(); #defaults to header_inc.php	
-	
-	echo 
+    get_header(); #defaults to header_inc.php
+
+	echo
 	'<script type="text/javascript" src="' . VIRTUAL_PATH . 'include/util.js"></script>
 	<script type="text/javascript">
 		function checkForm(thisForm)
@@ -72,20 +72,20 @@ function showForm()
 		}
 	</script>
 	<h3 align="center">' . smartTitle() . '</h3>
-	<p align="center">Please enter your name</p> 
+	<p align="center">Please enter your name</p>
 	<form action="' . THIS_PAGE . '" method="post" onsubmit="return checkForm(this);">
              ';
-  
-    
+
+
 		foreach($config->items as $item)
           {
-            //echo "<p>ID:$item->ID  Name:$item->Name</p>"; 
+            //echo "<p>ID:$item->ID  Name:$item->Name</p>";
             //echo '<p>Taco <input type="text" name="item_1" /></p>';
-              
+
               echo '<p>' . $item->Name . ' <input type="text" name="item_' . $item->ID . '" /></p>';
-              
-          }       
- 
+
+          }
+
           echo '
 				<p>
 					<input type="submit" value="Please Enter Your Name"><em>(<font color="red"><b>*</b> required field</font>)</em>
@@ -98,16 +98,16 @@ function showForm()
 
 function showData()
 {#form submits here we show entered name
-	
+
     //dumpDie($_POST);
      get_header(); #defaults to footer_inc.php
-	
-	
+
+
 	echo '<h3 align="center">' . smartTitle() . '</h3>';
-	
+
 	foreach($_POST as $name => $value)
     {//loop the form elements
-        
+
         //if form name attribute starts with 'item_', process it
         if(substr($name,0,5)=='item_')
         {
@@ -120,48 +120,30 @@ function showData()
 
 			/*
 				Here is where you'll do most of your work
-				Use $id to loop your array of items and return 
+				Use $id to loop your array of items and return
 				item data such as price.
-				
-				Consider creating a function to return a specific item 
+
+				Consider creating a function to return a specific item
 				from your items array, for example:
-				
+
 				$thisItem = getItem($id);
-				
-				Use $value to determine the number of items ordered 
+
+				Use $value to determine the number of items ordered
 				and create subtotals, etc.
-			
+
 			*/
             echo "<p>You ordered $value of item number $id</p>";
-            
+
         }
-        
-        
+
+
 
     }
-	
-	
-	
-	
+
+
+
+
 	echo '<p align="center"><a href="' . THIS_PAGE . '">RESET</a></p>';
 	get_footer(); #defaults to footer_inc.php
 }
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
