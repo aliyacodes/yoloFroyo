6b3083e199da70906f24e11dec9946278f35502b
brownae <aebrown9@gmail.com>
2017-02-07 10:20:14 -0800

    Starting off by simplifying files.

--- demo/itemsP-2.php
+++ demo/itemsP-2.php
@@ -1,11 +1,11 @@
 <?php
 //items4.php
 
-$myItem = new Item(1,"Taco","Our Tacos are awesome!",4.95);
-$myItem->addExtra("Sour Cream");
-$myItem->addExtra("Cheese");
-$myItem->addExtra("Guacamole");
-$config->items[] = $myItem;
+// $myItem = new Item(1,"Taco","Our Tacos are awesome!",4.95);
+// $myItem->addExtra("Sour Cream");
+// $myItem->addExtra("Cheese");
+// $myItem->addExtra("Guacamole");
+// $config->items[] = $myItem;
 
 $myItem = new Item(2,"Sundae","Our Sundaes are awesome!",3.95);
 $myItem->addExtra("Sprinkles");
@@ -13,12 +13,12 @@ $myItem->addExtra("Chocolate Sauce");
 $myItem->addExtra("Nuts");
 $config->items[] = $myItem;
 
-$myItem = new Item(3,"Salad","Our Salads are awesome!",5.95);
-$myItem->addExtra("Croutons");
-$myItem->addExtra("Bacon");
-$myItem->addExtra("Lemon Wedges");
-$myItem->addExtra("Avacado");
-$config->items[] = $myItem;
+// $myItem = new Item(3,"Salad","Our Salads are awesome!",5.95);
+// $myItem->addExtra("Croutons");
+// $myItem->addExtra("Bacon");
+// $myItem->addExtra("Lemon Wedges");
+// $myItem->addExtra("Avacado");
+// $config->items[] = $myItem;
 
 
 //create a counter to load the ids...
@@ -41,31 +41,20 @@ class Item
     public $Description = '';
     public $Price = 0;
     public $Extras = array();
-    
+
     public function __construct($ID,$Name,$Description,$Price)
     {
         $this->ID = $ID;
         $this->Name = $Name;
         $this->Description = $Description;
         $this->Price = $Price;
-        
+
     }#end Item constructor
-    
+
     public function addExtra($extra)
     {
         $this->Extras[] = $extra;
-        
+
     }#end addExtra()
 
 }#end Item class
-
-
-
-
-
-
-
-
-
-
-
